948667493dad7e24c78a100aae0adbe2bd0eed25
Lebedev Egor <egorikl@list.ru>
2021-06-14 22:45:18 +0300

    Refactor

--- src/main/java/ru/hse/servers/basic/BasicServer.java
+++ src/main/java/ru/hse/servers/basic/BasicServer.java
@@ -12,7 +12,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import static ru.hse.utils.Constants.PORT;
-import static ru.hse.utils.Utils.bubbleSort;
 
 public class BasicServer implements Server {
     private ServerSocket serverSocket = null;
@@ -46,69 +45,11 @@ public class BasicServer implements Server {
             while (!Thread.interrupted() && isWorking) {
                 try {
                     Socket clientSocket = serverSocket.accept();
-                    Client client = new Client(clientSocket);
+                    Client client = new Client(clientSocket, workerThreadPool);
                     clients.add(client);
                     client.processClient();
                 } catch (IOException ignored1) { }
             }
         } catch (IOException ignored) { }
-    }
-
-    private class Client {
-        private final Socket socket;
-        public final ExecutorService responseWriter = Executors.newSingleThreadExecutor();
-        public final ExecutorService requestReader = Executors.newSingleThreadExecutor();
-
-        private final DataInputStream inputStream;
-        private final DataOutputStream outputStream;
-
-        private volatile boolean working = true;
-
-        private Client(Socket socket) throws IOException {
-            this.socket = socket;
-            inputStream = new DataInputStream(socket.getInputStream());
-            outputStream = new DataOutputStream(socket.getOutputStream());
-        }
-
-        public void sendResponse(int[] data) {
-            responseWriter.submit(() -> {
-                try {
-                    Utils.writeArray(outputStream, data);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            });
-        }
-
-        public void processClient() {
-            requestReader.submit(() -> {
-                try {
-                    while(!Thread.interrupted() && working) {
-                        int[] data = Utils.readArray(inputStream);
-                        workerThreadPool.submit(() -> {
-                            bubbleSort(data);
-                            sendResponse(data);
-                        });
-                    }
-                } catch (IOException ignored) { }
-            });
-        }
-
-
-        public void close() {
-            working = false;
-            responseWriter.shutdown();
-            requestReader.shutdown();
-            try {
-                socket.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(socket, responseWriter, requestReader, inputStream, outputStream, working);
-        }
     }
 }
---
+++ src/main/java/ru/hse/servers/basic/Client.java
@@ -0,0 +1,73 @@
+package ru.hse.servers.basic;
+
+import ru.hse.utils.Utils;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static ru.hse.utils.Utils.bubbleSort;
+
+public class Client {
+    private final Socket socket;
+    private final ExecutorService workerThreadPool;
+    private final ExecutorService responseWriter = Executors.newSingleThreadExecutor();
+    private final ExecutorService requestReader = Executors.newSingleThreadExecutor();
+
+    private final DataInputStream inputStream;
+    private final DataOutputStream outputStream;
+
+    private volatile boolean working = true;
+
+    public Client(Socket socket, ExecutorService workerThreadPool) throws IOException {
+        this.socket = socket;
+        this.workerThreadPool = workerThreadPool;
+        inputStream = new DataInputStream(socket.getInputStream());
+        outputStream = new DataOutputStream(socket.getOutputStream());
+    }
+
+    private void sendResponse(int[] data) {
+        responseWriter.submit(() -> {
+            try {
+                Utils.writeArray(outputStream, data);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void processClient() {
+        requestReader.submit(() -> {
+            try {
+                while(!Thread.interrupted() && working) {
+                    int[] data = Utils.readArray(inputStream);
+                    workerThreadPool.submit(() -> {
+                        bubbleSort(data);
+                        sendResponse(data);
+                    });
+                }
+            } catch (IOException ignored) { }
+        });
+    }
+
+
+    public void close() {
+        working = false;
+        responseWriter.shutdown();
+        requestReader.shutdown();
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(socket, responseWriter, requestReader, inputStream, outputStream, working);
+    }
+}
